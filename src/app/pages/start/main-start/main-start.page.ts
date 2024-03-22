d8d6dfaafd66f6fc3565027c3a314bb6f97a9c7e
Fernando Avilésm <fernando.aviles.covena@gmail.com>
2024-03-21 22:17:22 -0500

    Menú main-start.

--- src/app/pages/start/main-start/main-start.page.ts
+++ src/app/pages/start/main-start/main-start.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationService } from 'src/app/services/navigation.service';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-main-start',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-start.page.scss'],
 })
 export class MainStartPage implements OnInit {
-
-  constructor() { }
+  nombre: string  = ''
+  constructor(
+    private irA: NavigationService,
+    private almacenar: StorageService,
+  ) { }
 
   ngOnInit() {
+    this.cargarNombre()
   }
 
+  async cargarNombre () {
+    this.nombre = await this.almacenar.obtener('nombres')
+    this.nombre = this.nombre.split(' ')[0]
+  }
 }
